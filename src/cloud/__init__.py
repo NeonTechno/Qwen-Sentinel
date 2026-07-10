c41b1294d7984282a30e582317667e296c739933
NeonTechnoX <89855167+NeonTechno@users.noreply.github.com>
2026-07-10 19:55:26 +0000

    Update __init__.py files to export new classes

--- src/cloud/__init__.py
+++ src/cloud/__init__.py
@@ -1,4 +1,5 @@
-from .alerts import AlertManager
+from .alerts import AlertManager, create_webhook_dispatcher
 from .memory import MemoryAgent
+from .qwen_client import QwenClient
 
-__all__ = ["AlertManager", "MemoryAgent"]
+__all__ = ["AlertManager", "MemoryAgent", "QwenClient", "create_webhook_dispatcher"]
--- src/edge/__init__.py
+++ src/edge/__init__.py
@@ -1,3 +1,5 @@
 from .detector import EdgeDetector
+from .ai_detector import AIDetector
+from .hybrid_detector import HybridDetector
 
-__all__ = ["EdgeDetector"]
+__all__ = ["EdgeDetector", "AIDetector", "HybridDetector"]
